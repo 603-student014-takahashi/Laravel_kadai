d653197587e7e4fd559f3da92a95d5981137ea0e
603-student014-takahashi <603-student014@la-study.com>
2025-05-14 09:07:04 +0900

    LV20

---
+++ memo-app/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>メモ帳アプリ</title>
+    </head>
+    <body>
+        <h1>メモ一覧</h1>
+        ・・・・<p><a href="{{ route('memos.create') }}">新規メモ作成</a></p>
+        <p>今日の買い物</p>
+        <p>明日の予定</p>
+    </body>
+</html>
